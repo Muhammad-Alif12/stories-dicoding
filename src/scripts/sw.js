018e59438306a2db1f48033270c1c7ae6d927a15
NyaaSe <rezaalif31@gmail.com>
2026-04-14 19:50:55 +0800

    Fix notif

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -85,19 +85,6 @@ registerRoute(
   }),
 );
 
-// self.addEventListener("push", (event) => {
-//   console.log("Service worker pushing...");
-
-//   async function chainPromise() {
-//     const data = await event.data.json();
-//     await self.registration.showNotification(data.title, {
-//       body: data.options.body,
-//     });
-//   }
-
-//   event.waitUntil(chainPromise());
-// });
-
 self.addEventListener("push", (event) => {
   async function showNotification() {
     let data = {};
